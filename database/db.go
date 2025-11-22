4b29e35709b2f9c665b566e441f9b78c334e475c
Hanlaevm5 <hanlaevm5@gmail.com>
2025-11-22 20:55:24 +0700

    Поменял host у дсн в бд

--- database/db.go
+++ database/db.go
@@ -10,8 +10,9 @@ import (
 var DB *gorm.DB
 
 func Connect() {
-	dsn := "host=localhost user=hanlaevm5 password=123 dbname=restaurant port=5432 sslmode=disable"
+	dsn := "host=host.docker.internal user=hanlaevm5 password=123 dbname=restaurant port=5432 sslmode=disable"
 	db, err := gorm.Open(postgres.Open(dsn), &gorm.Config{})
+
 	if err != nil {
 		log.Fatal("Ошибка подключения к базе данных:", err)
 	}
